a3be283686232bffb7a93ef55b077b10457b0124
Grahame Grieve <grahameg@gmail.ccom>
2024-09-23 11:44:24 -0400

    add next version of UDS+

---
+++ source/guides/hrsa/uds-plus/STU1.1/StructureDefinition/uds-plus-deidentify-operation-data-urls-parameter/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.html');
+else 
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1.1/StructureDefinition-uds-plus-deidentify-operation-data-urls-parameter.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ source/guides/hrsa/uds-plus/STU1/CodeSystem/udsplus-agriculture-worker-status-codes/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.html');
+else 
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU1/CodeSystem-udsplus-agriculture-worker-status-codes.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
