d1118b1ec530dd062817dcb09a499fc04572d8c6
Grahame Grieve <grahameg@gmail.ccom>
2025-07-04 09:28:30 +0200

    hrsa

---
+++ source/guides/hrsa/uds-plus/ValueSet/uds-plus-sex-codes/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.html');
+else 
+  Redirect('http://fhir.org/guides/hrsa/uds-plus/STU2/ValueSet-uds-plus-sex-codes.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
